10141d655647b35cd6448d1c224dbc643a1626cf
ドペプシ <@DESKTOP-MHULA6N>
2021-11-30 22:58:17 +0900

    Employeeモデル、EmployeeViewモデル作成

---
+++ src/main/java/views/EmployeeView.java
@@ -0,0 +1,34 @@
+package views;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+//従業員情報について画面の入力値、出力地を扱うviewモデル
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class EmployeeView{
+
+    private Integer id;
+
+    private String code;
+
+    private String name;
+
+    private String password;
+
+    private Integer adminFlag;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+    private Integer deleteFlag;
+
+}
